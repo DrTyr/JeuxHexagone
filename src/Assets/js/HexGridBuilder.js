fcd9ed4e32e2a640f07d22466ed1fa1fed93e24b
Aloïs Hachet <alois@Speedy.local>
2021-04-05 18:07:07 +0200

    Add new test script from web

---
+++ src/Assets/js/HexGridBuilder.js
@@ -0,0 +1,16 @@
+var HexGridBuilder = function (width, depth, margin) {
+
+};
+
+HexGridBuilder.prototype.calculateInitialPosition = function () {
+
+};
+
+
+HexGridBuilder.prototype.getWorldCoordinate = function (x, y, z) {
+
+};
+
+HexGridBuilder.prototype.generate = function (scene) {
+
+};
