2273c538ad2e1f8a29f619948be75e5a1aa7dbba
Aloïs Hachet <alois@Speedy.local>
2021-04-09 14:51:49 +0200

    (WIP) Redoing the math

--- src/HexagoneGridCalculator.jsx
+++ src/HexagoneGridCalculator.jsx
@@ -9,73 +9,74 @@ const numberHexagoneInRow = 10;
 
 //Generate all summit coordonate, clockwise, starting with the East one
 function getOneHexagoneAllSummitCoordinate(hexagone) {
-  for (let i = 0; i <= 6; i++) {
-    let angle = (PI / 180) * -60 * i;
-    hexagone.coordSommit.x[i] =
-      hexagone.coordCenter.x + hexagone.size * cos(angle);
-    hexagone.coordSommit.y[i] =
-      hexagone.coordCenter.y + hexagone.size * sin(angle);
-  }
-  return hexagone.coordSommit;
+    for (let i = 0; i <= 6; i++) {
+        let angle = (PI / 180) * -60 * i;
+        hexagone.coordSommit.x[i] =
+            hexagone.coordCenter.x + hexagone.size * cos(angle);
+        hexagone.coordSommit.y[i] =
+            hexagone.coordCenter.y + hexagone.size * sin(angle);
+    }
+    return hexagone.coordSommit;
 }
 
 //Function the generate the Grid as object gridObject of object hexagoneObject
 export function generateEntireGrid() {
-  let grid = {
-    firstHexagoneCenter: { x: 150, y: 150 },
-    hexagones: [],
-    numberHexagoneInColumn: numberHexagoneInColumn,
-    numberHexagoneInRow: numberHexagoneInRow,
-    //numberHexagoneInGrid: this.numberHexagoneInColumn * this.numberHexagoneInRow
-  };
+    let grid = {
+        firstHexagoneCenter: { x: 150, y: 150 },
+        hexagones: [],
+        numberHexagoneInColumn: numberHexagoneInColumn,
+        numberHexagoneInRow: numberHexagoneInRow,
+        //numberHexagoneInGrid: this.numberHexagoneInColumn * this.numberHexagoneInRow
+    };
 
-  let hexagoneIndice = 0;
+    let hexagoneIndice = 0;
 
-  for (let i = 0; i <= grid.numberHexagoneInRow; i++) {
-    grid.hexagones[i] = [];
+    for (let i = 0; i <= grid.numberHexagoneInRow; i++) {
+        grid.hexagones[i] = [];
 
-    for (let j = 0; j <= grid.numberHexagoneInColumn; j++) {
-      let hexagone = {
-        indice: 0,
-        coordInGrid: { x: 0, y: 0 },
-        coordSommit: { x: [], y: [] },
-        coordCenter: { x: 0, y: 0 },
-        size: hexagoneSize,
-      };
+        for (let j = 0; j <= grid.numberHexagoneInColumn; j++) {
+            let hexagone = {
+                indice: 0,
+                coordInGrid: { x: 0, y: 0 },
+                coordSommit: { x: [], y: [] },
+                coordCenter: { x: 0, y: 0 },
+                size: hexagoneSize,
+            };
 
-      //Testing if drawing odd or even column i
-      if (i % 2 === 0) {
-        hexagone.coordCenter.x =
-          grid.firstHexagoneCenter.x +
-          i * 3 * hexagoneSize +
-          1.5 * hexagoneSize;
-        hexagone.coordCenter.y =
-          grid.firstHexagoneCenter.y + sin((60 * PI) / 180) * hexagoneSize;
-      } else {
-        hexagone.coordCenter.x =
-          grid.firstHexagoneCenter.x + i * 3 * hexagoneSize;
-        hexagone.coordCenter.y = grid.firstHexagoneCenter.y;
-      }
+            //Testing if drawing odd or even column i
+            if (i % 2 === 0) {
+                hexagone.coordCenter.x =
+                    grid.firstHexagoneCenter.x +
+                    i * 3 * hexagoneSize +
+                    1.5 * hexagoneSize;
+                hexagone.coordCenter.y =
+                    grid.firstHexagoneCenter.y + sin((60 * PI) / 180) * hexagoneSize;
+            } else {
+                hexagone.coordCenter.x =
+                    grid.firstHexagoneCenter.x +
+                    i * 3 * hexagoneSize;
+                hexagone.coordCenter.y = grid.firstHexagoneCenter.y;
+            }
 
-      // WTF tu écrases les valeurs hexagone.coordCenter.x et hexagone.coordCenter.y
-      //   hexagone.coordCenter.x =
-      //     grid.firstHexagoneCenter.x + j * 3 * hexagoneSize;
-      //   hexagone.coordCenter.y =
-      //     grid.firstHexagoneCenter.y +
-      //     j * 2 * sin((60 * PI) / 180) * hexagoneSize;
+            // WTF tu écrases les valeurs hexagone.coordCenter.x et hexagone.coordCenter.y
+            //   hexagone.coordCenter.x =
+            //     grid.firstHexagoneCenter.x + j * 3 * hexagoneSize;
+            //   hexagone.coordCenter.y =
+            //     grid.firstHexagoneCenter.y +
+            //     j * 2 * sin((60 * PI) / 180) * hexagoneSize;
 
-      hexagone.coordSommit = getOneHexagoneAllSummitCoordinate(hexagone);
+            hexagone.coordSommit = getOneHexagoneAllSummitCoordinate(hexagone);
 
-      //How to change both value at once ?
-      hexagone.coordInGrid.x = i;
-      hexagone.coordInGrid.y = j;
+            //How to change both value at once ?
+            hexagone.coordInGrid.x = i;
+            hexagone.coordInGrid.y = j;
 
-      hexagone.indice = hexagoneIndice++;
+            hexagone.indice = hexagoneIndice++;
 
-      grid.numberHexagoneInGrid++;
-      grid.hexagones[i][j] = hexagone;
+            grid.numberHexagoneInGrid++;
+            grid.hexagones[i][j] = hexagone;
+        }
     }
-  }
 
-  return grid;
+    return grid;
 }
--- src/HexagoneGridGenerator.jsx
+++ src/HexagoneGridGenerator.jsx
@@ -4,61 +4,61 @@ import "./canvas.css";
 import { generateEntireGrid } from "./HexagoneGridCalculator";
 
 function Canvas() {
-  const hexagoneCanvas = useRef(null);
+    const hexagoneCanvas = useRef(null);
 
-  const [canvasWidth, setCanvasWidth] = useState(800);
-  const [canvasHeight, setCanvasHeight] = useState(600);
+    const [canvasWidth, setCanvasWidth] = useState(800);
+    const [canvasHeight, setCanvasHeight] = useState(600);
 
-  useEffect(() => {
-    draw();
-  }, []);
+    useEffect(() => {
+        draw();
+    }, []);
 
-  function clearCanvas() {
-    const canvas = hexagoneCanvas.current;
-    const context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  function draw() {
-    clearCanvas();
-    drawGrid();
-  }
-
-  function drawGrid() {
-    const ctx = hexagoneCanvas.current.getContext("2d");
-    ctx.fillStyle = getRandomColor();
-    let grid = generateEntireGrid();
-    // debugger;
-
-    console.log(grid);
-
-    for (let i = 0; i <= grid.numberHexagoneInRow; i++) {
-      for (let j = 0; j <= grid.numberHexagoneInColumn; j++) {
-        ctx.beginPath();
-        for (let z = 0; z <= 6; z++) {
-          ctx.lineTo(
-            grid.hexagones[i][j].coordSommit.x[z],
-            grid.hexagones[i][j].coordSommit.y[z]
-          );
-        }
-
-        ctx.stroke();
-        ctx.fill();
-        ctx.closePath();
-      }
+    function clearCanvas() {
+        const canvas = hexagoneCanvas.current;
+        const context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
     }
-  }
 
-  return (
-    <div>
-      {" "}
-      <canvas
-        ref={hexagoneCanvas}
-        width={canvasWidth}
-        height={canvasHeight}
-      ></canvas>
-    </div>
-  );
+    function draw() {
+        clearCanvas();
+        drawGrid();
+    }
+
+    function drawGrid() {
+        const ctx = hexagoneCanvas.current.getContext("2d");
+        ctx.fillStyle = getRandomColor();
+        let grid = generateEntireGrid();
+        // debugger;
+
+        console.log(grid);
+
+        for (let i = 0; i <= grid.numberHexagoneInRow; i++) {
+            for (let j = 0; j <= grid.numberHexagoneInColumn; j++) {
+                ctx.beginPath();
+                for (let z = 0; z <= 6; z++) {
+                    ctx.lineTo(
+                        grid.hexagones[i][j].coordSommit.x[z],
+                        grid.hexagones[i][j].coordSommit.y[z]
+                    );
+                }
+
+                ctx.stroke();
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+
+    return (
+        <div>
+            {" "}
+            <canvas
+                ref={hexagoneCanvas}
+                width={canvasWidth}
+                height={canvasHeight}
+            ></canvas>
+        </div>
+    );
 }
 export default Canvas;
 
