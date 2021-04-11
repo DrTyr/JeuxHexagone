ec29172fc3dc577dd2fe184ec531475ab2992380
Alois <alois.hachet@gmail.com>
2021-04-11 18:49:08 +0200

    Add color changing on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import "./App.css";
-import React from "react";
-import Canvas from "./CanvasGridGenerator";
-import { getHexagonCoordPointInString, getHexagonColor } from "./SvgGridGenerator";
+import React, { useState, useEffect } from "react";
+//import Canvas from "./CanvasGridGenerator";
+import { getHexagonCoordPointInString } from "./SvgGridGenerator";
 import { generateEntireGrid } from "./HexagonGridCalculator";
+import { getRandomColor } from "./library";
 
 export function App() {
 
-  let grid = generateEntireGrid();
+  const [grid, setGrid] = useState(generateEntireGrid());
+
+  // let grid = generateEntireGrid();
   // let hexagonCoordForSvg = getHexagonCoordPointInString(grid, 0, 0);
   // let hexagonColor = grid.hexagons[0][0].color;
+
+  useEffect(() => {
+
+    console.log("REDRAW YOUHOU")
+
+  }, [grid]);
+
+  function prepareHexagonesForSVG() {
+
+
+    //return grid.hexagons.map((hexagons, i) => hexagons.map((hexagon, j) => {
+    return grid.hexagons.map((hexagons) => hexagons.map((hexagon) => {
+
+      let hexagonCoordForSvg = getHexagonCoordPointInString(hexagon);
+
+      //let hexagonColor = grid.hexagons[i][j].color;
+      let hexagonColor = hexagon.color;
+
+      let hexagoneindiceForBalise = `indice${hexagon.indice}`
+
+      return <polygon onClick={() => {
+        let grid2 = { ...grid };
+        grid2.hexagons[hexagon.coordInGrid.x][hexagon.coordInGrid.y].color = getRandomColor();
+        setGrid(grid2);
+      }} key={hexagoneindiceForBalise} points={hexagonCoordForSvg} fill={hexagonColor} />
+
+    }))
+
+  }
+
+  //Exercie
 
 
   return (
@@ -18,7 +52,7 @@ export function App() {
         {/* <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="red" /> */}
         {/* <polygon points={hexagoneCoordForSvg} fill={hexagonColor} /> */}
 
-        {prepareHexagonesForSVG(grid)}
+        {prepareHexagonesForSVG()}
 
       </svg>
     </div>
@@ -30,24 +64,3 @@ export function App() {
 
 export default App;
 
-
-function prepareHexagonesForSVG(grid) {
-
-
-  //return grid.hexagons.map((hexagons, i) => hexagons.map((hexagon, j) => {
-  return grid.hexagons.map((hexagons) => hexagons.map((hexagon) => {
-
-    let hexagonCoordForSvg = getHexagonCoordPointInString(hexagon);
-
-    //let hexagonColor = grid.hexagons[i][j].color;
-    let hexagonColor = hexagon.color;
-
-    let hexagoneindiceForBalise = `indice${hexagon.indice}`
-
-    return <polygon onClick={() => { console.log(hexagon) }} key={hexagoneindiceForBalise} points={hexagonCoordForSvg} fill={hexagonColor} />
-
-
-  }))
-
-
-}
